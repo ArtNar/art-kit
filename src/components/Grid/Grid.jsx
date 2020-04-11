d41db79107956676abb1110a3158d5250285d9a3
Artem Narozhniy <artemnarozhniy@gmail.com>
2020-04-12 01:47:19 +0200

    grid refactor

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -3,77 +3,48 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import _cn from '../../utils/cn';
 
-const types = [false, 'auto', true, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
-const getClassName = (props) => Object.keys(props)
-    .reduce((className, key) => (props[key] ? `${className} col-${key}-${props[key]}`.trim() : className), '');
+const types = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const cn = _cn('grid');
 
 const Grid = React.forwardRef(({
-    lg = false,
-    md = false,
-    sm = false,
-    xl = false,
-    xs = false,
-    spacing = 0,
-    container,
-    direction = 'row',
-    justify = 'flex-start',
-    alignItems = 'stretch',
-    item,
+    className,
+    children,
+    xs,
+    sm,
+    md,
+    lg,
+    xl,
+    padded,
+    type,
     ...rest
 }, ref) => (
     <div
-        ref={ref}
-        className={cx(
-            item
-                ? getClassName({
-                    lg,
-                    md,
-                    sm,
-                    xl,
-                    xs,
-                })
-                : 'row',
-            cn({
-                lg,
-                md,
-                sm,
-                xl,
-                xs,
-                spacing,
-                container,
-                direction,
-                justify,
-                alignItems,
-                item,
-            }),
-        )}
         {...rest}
-    />
+        ref={ref}
+        className={cx(cn({
+            lg,
+            md,
+            sm,
+            xl,
+            xs,
+            type,
+        }), className)}
+    >
+        {children}
+    </div>
 ));
 
 Grid.propTypes = {
+    className: PropTypes.string,
     children: PropTypes.node,
-    container: PropTypes.bool,
-    item: PropTypes.bool,
+    padded: PropTypes.bool,
+    type: PropTypes.oneOf(['container', 'column']),
     lg: PropTypes.oneOf(types),
     md: PropTypes.oneOf(types),
     sm: PropTypes.oneOf(types),
     xl: PropTypes.oneOf(types),
     xs: PropTypes.oneOf(types),
-    spacing: PropTypes.number,
-    direction: PropTypes.oneOf(['row', 'row-reverse', 'column', 'column-reverse']),
-    justify: PropTypes.oneOf([
-        'flex-start',
-        'center',
-        'flex-end',
-        'space-between',
-        'space-around',
-        'space-evenly',
-    ]),
-    alignItems: PropTypes.oneOf(['flex-start', 'center', 'flex-end', 'stretch', 'baseline']),
 };
 
 export default Grid;
--- src/components/Grid/README.md
+++ src/components/Grid/README.md
@@ -1,4 +1,20 @@
 Grid:
 ```jsx
+<Grid
+    type='container'
+>
+    <Grid
+        type='column'
+        xs={7}
+    >
+        column 1
+    </Grid>
+    <Grid
+        type='column'
+        xs={5}
+    >
+        column 2
+    </Grid>
+</Grid>
 
 ```
